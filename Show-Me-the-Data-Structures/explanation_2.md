9d9d5e9a222f94f55f97897aa007977fefde95de
hirooiida <hiroo.iida@outlook.com>
2021-04-09 10:06:21 +0900

    completed problem_2

---
+++ Show-Me-the-Data-Structures/explanation_2.md
@@ -0,0 +1,7 @@
+# Reason
+Recursion technique is used instead of ```for``` loop for code simplicity.
+
+# Efficiency
+Time complexity is O(n) because it checks all the paths one by one. Calculation steps can be linearly incleased according to the number of paths.
+
+Space efficiency is O(n). ```output``` list is an only container which can store all the paths in worst case.
---
+++ Show-Me-the-Data-Structures/problem_2.py
@@ -0,0 +1,44 @@
+import os
+
+def find_files(suffix, path):
+    """
+    Find all files beneath path with file name suffix.
+
+    Note that a path may contain further subdirectories
+    and those subdirectories may also contain further subdirectories.
+
+    There are no limit to the depth of the subdirectories can be.
+
+    Args:
+      suffix(str): suffix if the file name to be found
+      path(str): path of the file system
+
+    Returns:
+       a list of paths
+    """
+
+    if suffix == "":
+        return None
+
+    if path == "":
+        return None
+
+    output = list()
+
+    if os.path.isfile(path):
+        if path.endswith(suffix):
+            return [path]
+    else:
+        files = os.listdir(path)
+        for file in files:
+            output += find_files(suffix, path + "/" + file)
+    
+    return output
+
+print("\n==== Test cases ====")
+print(find_files(".c", "./testdir"))
+# ['./testdir/subdir3/subsubdir1/b.c', './testdir/t1.c', './testdir/subdir5/a.c', './testdir/subdir1/a.c']
+print(find_files(".h", "./testdir"))
+#['./testdir/subdir3/subsubdir1/b.h', './testdir/subdir5/a.h', './testdir/t1.h', './testdir/subdir1/a.h']
+print(find_files(".cpp", "./testdir"))
+# []
